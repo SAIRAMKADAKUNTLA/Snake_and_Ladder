615eb73fc8664d5af99e7c8092efd02a172ea31f
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-01 15:07:25 +0530

    Added:position of the player

---
+++ Snake__Ladder/Snake__Ladder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake__Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to snake ladder game");
+            SnakeLadderGame snakeLadderGame = new SnakeLadderGame();
+            snakeLadderGame.Position();
+        }
+    }
+}
---
+++ Snake__Ladder/Snake__Ladder/SnakeLadderGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake__Ladder
+{
+    public class SnakeLadderGame
+    {
+        /// UC1-Position
+        int StartPosition = 0;
+        public void Position()
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at position is"+" "+StartPosition);
+        }
+
+
+
+    }
+}
